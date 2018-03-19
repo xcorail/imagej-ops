d007f09118267191fa9d6b5000f9134294353fbb
Ellen T Arena <arenaellen@gmail.com>
2018-03-19 16:51:48 -0400

    Adding Pixel-wise code: WIP
    Added new WeightedMergeSort and AdaptiveSmoothedKendallTau for
    pixel-wise colocalization analysis.

---
+++ src/main/java/net/imagej/ops/coloc/adaptiveKTau/AdaptiveKTau.java
@@ -0,0 +1,105 @@
+/*-
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2014 - 2017 ImageJ developers.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package net.imagej.ops.coloc.adaptiveKTau;
+
+import net.imagej.ops.Contingent;
+import net.imagej.ops.Ops;
+import net.imagej.ops.coloc.ColocUtil;
+import net.imagej.ops.special.function.AbstractBinaryFunctionOp;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.util.IterablePair;
+import net.imglib2.util.Pair;
+
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * This algorithm calculates
+ *
+ * @param <T>
+ *            Type of the first image
+ * @param <U>
+ *            Type of the second image
+ */
+@Plugin(type = Ops.Coloc.SACA.class)
+public class AdaptiveKTau<T extends RealType<T>, U extends RealType<U>>
+		extends AbstractBinaryFunctionOp<Iterable<T>, Iterable<U>, Double> implements Ops.Coloc.ICQ, Contingent {
+
+	@Parameter(required = false)
+	private DoubleType mean1;
+
+	@Parameter(required = false)
+	private DoubleType mean2;
+
+	@Override
+	public Double calculate(final Iterable<T> image1, final Iterable<U> image2) {
+
+		final Iterable<Pair<T, U>> samples = new IterablePair<>(image1, image2);
+
+		final double m1 = mean1 == null ? computeMeanOf(image1) : mean1.get();
+		final double m2 = mean2 == null ? computeMeanOf(image2) : mean2.get();
+
+		// variables to count the positive and negative results
+		// of Li's product of the difference of means.
+		long numPositiveProducts = 0;
+		long numNegativeProducts = 0;
+		// iterate over image
+		for (final Pair<T, U> value : samples) {
+
+			final double ch1 = value.getA().getRealDouble();
+			final double ch2 = value.getB().getRealDouble();
+
+			final double productOfDifferenceOfMeans = (m1 - ch1) * (m2 - ch2);
+
+			// check for positive and negative values
+			if (productOfDifferenceOfMeans < 0.0)
+				++numNegativeProducts;
+			else
+				++numPositiveProducts;
+		}
+
+		/*
+		 * calculate Li's ICQ value by dividing the amount of "positive pixels"
+		 * to the total number of pixels. Then shift it in the -0.5,0.5 range.
+		 */
+		final double icqValue = ((double) numPositiveProducts / (double) (numNegativeProducts + numPositiveProducts))
+				- 0.5;
+		return icqValue;
+	}
+
+	private <V extends RealType<V>> double computeMeanOf(final Iterable<V> in) {
+		return ops().stats().mean(in).getRealDouble();
+	}
+
+	@Override
+	public boolean conforms() {
+		return ColocUtil.sameIterationOrder(in1(), in2());
+	}
+}
